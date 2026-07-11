e9558df0ac5296caa13e626c6e3599dfef933383
DanantST <akintolapeace001@gmail.com>
2026-07-11 17:30:25 +0100

    Disable global getaddrinfo IPv4 DNS override to test if it resolves SSL handshake timeouts

--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -9,7 +9,7 @@ import socket
 orig_getaddrinfo = socket.getaddrinfo
 def getaddrinfo_ipv4(host, port, family=0, type=0, proto=0, flags=0):
     return orig_getaddrinfo(host, port, socket.AF_INET, type, proto, flags)
-socket.getaddrinfo = getaddrinfo_ipv4
+# socket.getaddrinfo = getaddrinfo_ipv4  # Disabled override
 # Set process timezone to Africa/Lagos to match the ESP32 device's WAT timezone
 if hasattr(time, "tzset"):
     os.environ['TZ'] = 'Africa/Lagos'
